bc8869d2b09754b4d32dd408dab7cc4fab5906e9
busraUnver <busra.unver@ug.bilkent.edu.tr>
2021-07-26 11:37:03 +0300

    init

---
+++ rest/src/main/java/com/rest/App.java
@@ -0,0 +1,17 @@
+package com.rest;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Simple java maven app by busra!");
+    }
+}
